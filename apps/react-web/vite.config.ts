2efeb3015951609ceb210e5a39a5366f8c1f82e4
Sivut Mekareeya <sivut_mek@outlook.com>
2025-02-11 17:17:13 +0700

    adjust vite app build config

--- apps/react-web/vite.config.ts
+++ apps/react-web/vite.config.ts
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: "esbuild", // Fast minification
+    rollupOptions: {
+      treeshake: true, // Removes unused code
+    },
+  },
+  esbuild: {
+    treeShaking: true,
+    drop: ["console", "debugger"], // Remove console logs & debuggers in production
+  },
   server: {
     port: 3001,
   },
