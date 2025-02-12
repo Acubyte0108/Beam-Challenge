3fd0c05fd6dc6284b5f3d1c0b15827cb5adc229b
Sivut Mekareeya <sivut_mek@outlook.com>
2025-02-13 03:04:14 +0700

    add custom navbar

---
+++ apps/web/app/news/page.tsx
@@ -0,0 +1,9 @@
+export default function NewsRoom() {
+    return (
+      <div className="flex items-center justify-center min-h-svh">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <h1 className="text-2xl font-bold">This is news room page</h1>
+        </div>
+      </div>
+    );
+  }
