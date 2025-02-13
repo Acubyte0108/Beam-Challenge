bbf6a11398d645d277eefa026b9fc71c3b79f14f
Sivut Mekareeya <sivut_mek@outlook.com>
2025-02-14 03:26:15 +0700

    adjust page layout style

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,14 +4,12 @@ import { BeamLogoLight } from "@workspace/ui/components/beam-components/icons"
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
+      <div className="flex flex-col items-center justify-center gap-4 min-h-full">
         <h1 className="text-2xl font-bold">Hello World</h1>
         <p className="text-lg">Real Project</p>
         <Button size="sm">Button</Button>
         <BeamButton variantType="outline">Beam Button</BeamButton>
         <BeamLogoLight />
       </div>
-    </div>
   )
 }
