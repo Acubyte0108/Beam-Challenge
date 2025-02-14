f72ce45ae8ea0dd3a870187aa462c4d8a6f18214
Sivut Mekareeya <sivut_mek@outlook.com>
2025-02-14 18:44:44 +0700

    adjust style

--- apps/web/app/news/page.tsx
+++ apps/web/app/news/page.tsx
@@ -3,7 +3,7 @@ import { NewsSection } from "@/components/news-section";
 
 export default function NewsRoom() {
   return (
-    <div className="flex flex-col items-center gap-y-4 min-h-full bg-[#F6F8FA]">
+    <div className="flex flex-col items-center gap-y-4 min-h-svh bg-[#F6F8FA]">
       <div className="w-full bg-gradient-to-r from-blue-600 to-purple-400 flex items-center justify-center text-white md:text-4xl text-2xl font-semibold h-48 md:h-64 lg:h-80">
         News room
       </div>
