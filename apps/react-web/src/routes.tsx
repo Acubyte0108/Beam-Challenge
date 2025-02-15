cc6cde666d8d2ef25a0535efc30e4aeed74f6481
Sivut Mekareeya <sivut_mek@outlook.com>
2025-02-15 20:13:00 +0700

    mimic about us page from nextjs app

--- apps/react-web/src/routes.tsx
+++ apps/react-web/src/routes.tsx
@@ -4,12 +4,14 @@ import App from './App';
 import Home from './pages/home';
 import NotFound from './pages/not-found';
 import NewsRoom from './pages/news-room';
+import AboutUs from './pages/about-us'
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />} errorElement={<NotFound />}>
       <Route index element={<Home />} />
       <Route path="news" element={<NewsRoom />} />
+      <Route path="about" element={<AboutUs />} />
     </Route>
   )
 );
